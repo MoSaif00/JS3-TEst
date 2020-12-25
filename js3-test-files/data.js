626551a2ad1bfd819778548b2da45f010e9b5d12
Mohammed Saif <69274977+MoSaif00@users.noreply.github.com>
2020-12-25 02:00:31 +0100

    Add files via upload

--- js3-test-files/data.js
+++ js3-test-files/data.js
@@ -1,105 +1,80 @@
-// data.forEach((element) => {
-//   getTheHouseListDiv.innerHTML = `<div class="got-house">
-//   <h1 class="got-house__title">${element.name}</h1>
-//   <span class="got-house__current-lord">${element.currentLord.name}</span>
-// </div>`;
-
-function main() {
-  /*
+/*
   This list gives the ids of all the great houses that we want to look up.
   It should be inserted into the html above all the other files to ensure it is available
 */
 
-  const houseIdList = [
-    7, // Eyrie
-    17, // Baratheon
-    169, // Greyjoy
-    229, // Lannister
-    362, // Stark
-    378, // Targaryen
-    395, // Tully
-    398, // Tyrell
-  ];
+const houseIdList = [
+  7, // Eyrie
+  17, // Baratheon
+  169, // Greyjoy
+  229, // Lannister
+  362, // Stark
+  378, // Targaryen
+  395, // Tully
+  398, // Tyrell
+];
 
-  const killLordButton = document.getElementById('kill-random-lord-button');
-  const getTheHouseListDiv = document.getElementById('got-house-list');
+const killLordButton = document.getElementById('kill-random-lord-button');
+const getTheHouseListDiv = document.getElementById('got-house-list');
+const houseDataArray = [];
+const killedLords = [];
 
-  function fetchedHouseData() {
-    houseIdList.forEach((houseId) => {
-      fetch(`https://anapioficeandfire.com/api/houses/${houseId}`)
-        .then((response) => {
-          const data = response.json();
-          return data;
-        })
-        .then((data) => {
-          const createHouseDiv = document.createElement('div');
-          createHouseDiv.setAttribute('class', 'got-house');
-          const createHouseTitle = document.createElement('h1');
-          createHouseTitle.setAttribute('class', 'got-house__title');
-          getTheHouseListDiv.appendChild(createHouseDiv);
-          createHouseTitle.innerText = data.name;
-          createHouseDiv.appendChild(createHouseTitle);
-          const currentLordNameSpanElement = document.createElement('span');
-          currentLordNameSpanElement.setAttribute(
-            'class',
-            'got-house__current-lord'
-          );
-          fetchLordData(houseId, currentLordNameSpanElement, data);
-          createHouseDiv.appendChild(currentLordNameSpanElement);
-        })
-        .catch((error) => {
-          console.log('err', error);
-        });
-    });
-  }
-  fetchedHouseData();
-
-  function fetchLordData(id, lordName, swornData) {
-    fetch(`https://anapioficeandfire.com/api/characters/${id}`)
-      .then((response) => {
-        const data = response.json();
-        return data;
-      })
-      .then((data) => {
-        lordName.innerText = data.name;
-        if (data.name.length === 0) {
-          const randomChoiceOfSworn = Math.floor(
-            Math.random() * swornData.swornMembers.length
-          );
-          fetch(`${swornData.swornMembers[randomChoiceOfSworn]}`)
-            .then((response) => {
-              const data = response.json();
-              return data;
-            })
-            .then((res) => {
-              lordName.innerText = res.name;
-            });
-        }
-      })
-      .catch((error) => {
-        console.log('err', error);
-      });
-  }
-
-  function killLord() {
-    const currentLordNames = document.querySelector('.got-house__current-lord');
-
-    const randomKill = Math.floor(Math.random() * currentLordNames.length);
-    houseIdList.forEach((id) => {
-      fetch(`https://anapioficeandfire.com/api/houses/${id}`)
-        .then((response) => {
-          const data = response.json();
-          return data;
-        })
-        .then((result) => {
-          const randomSelect = Math.floor(
-            Math.random() * result.swornMembers.length.name
-          );
-          currentLordNames.innerText = randomSelect;
-        });
-    });
-  }
-  killLordButton.addEventListener('click', killLord);
+async function fetchData(url) {
+  const response = await fetch(url);
+  return await response.json();
 }
 
-window.onload = main;
+async function fetchedHouseData() {
+  houseIdList.forEach(async (id) => {
+    const houseData = await fetchData(
+      `https://anapioficeandfire.com/api/houses/${id}`
+    );
+
+    const {name, currentLord} = houseData;
+    houseDataArray.push({...houseData, id});
+    let currentLordData;
+    if (currentLord === '') {
+      currentLordData = {
+        name: 'Dead',
+      };
+    } else {
+      currentLordData = await fetchData(currentLord);
+    }
+
+    const createHouseDiv = document.createElement('div');
+    createHouseDiv.setAttribute('class', 'got-house');
+    const createHouseTitle = document.createElement('h1');
+    createHouseTitle.setAttribute('class', 'got-house__title');
+    getTheHouseListDiv.appendChild(createHouseDiv);
+    createHouseTitle.innerText = name;
+    createHouseDiv.appendChild(createHouseTitle);
+    const currentLordNameSpanElement = document.createElement('span');
+    currentLordNameSpanElement.setAttribute('class', 'got-house__current-lord');
+    currentLordNameSpanElement.id = id;
+    currentLordNameSpanElement.innerText = currentLordData.name;
+    createHouseDiv.appendChild(currentLordNameSpanElement);
+  });
+}
+
+function getRandomElement(array) {
+  const randomId = Math.floor(Math.random() * array.length);
+  return array[randomId];
+}
+
+async function killLordRandomly() {
+  const randomHouseId = getRandomElement(houseIdList);
+  const houseData = houseDataArray.find((house) => house.id === randomHouseId);
+
+  let randomSwornMember;
+  randomSwornMember = getRandomElement(houseData.swornMembers);
+  if (!killedLords.includes(randomSwornMember)) {
+    killedLords.push(randomSwornMember);
+  }
+  const swornMemberData = await fetchData(randomSwornMember);
+
+  const lordElement = document.getElementById(randomHouseId);
+  lordElement.innerText = swornMemberData.name;
+}
+
+window.addEventListener('load', fetchedHouseData);
+killLordButton.addEventListener('click', killLordRandomly);
